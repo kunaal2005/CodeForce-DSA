43826fcbdb268d69e347ed5867cf45eea93f86c1
Kunal <kunalupadhayay10@gmail.com>
2023-11-11 14:45:34 +0530

    11-11-23

---
+++ Kunal/Xenia_and_Ringroad.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int initial=1;
+    long long time=0;
+    for (int i = 0; i < m; i++)
+    {
+        int present;
+        cin >> present; 
+        if(present>=initial)
+        {
+            time+= present- initial;
+        }else time+= n- (initial-present);
+        initial=present;
+    }
+    cout << time;
+    return 0;
+}
